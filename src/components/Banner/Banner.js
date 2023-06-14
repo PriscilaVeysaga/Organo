cdbdb47b190b241f3baf6db3d5ec0fb128a7aa34
Priscila Veysaga <pri_veysaga@hotmail.com>
2023-06-14 01:42:29 -0300

    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="./imagens/fantasmaBanner2.png" alt="banner principal"/>
+        </header>
+    )
+}
+
+export default Banner
---
+++ src/components/Rodape/index.js
@@ -0,0 +1,13 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className="footer">
+        <section>
+            <p>
+                Desenvolvido por Priscila Veysaga.
+            </p>
+        </section>
+    </footer>)
+}
+
+export default Rodape
